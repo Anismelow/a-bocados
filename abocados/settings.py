b286fde294c8a45c203602a89fee8df3f8a133ce
anismelow <anyellmendozalopez@gmail.com>
2025-04-07 19:48:01 +0200

    se elimino los archivos docker

--- abocados/settings.py
+++ abocados/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 # Application definition
 
 INSTALLED_APPS = [
